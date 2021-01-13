c3c00d3aeaf05f433f1ab33fdeb3fc9927617fa2
Viktor Fetsenets <forestforgeron@gmail.com>
2021-01-13 21:13:17 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20210113-205643\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20210113-211237\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
